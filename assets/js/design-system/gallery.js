c85bb13b018464fc3ac4a02bf1b10ac4ffb2b92d
Olivia206 <olivia.simonet206@gmail.com>
2026-09-07 14:18:05 +0200

    project's blocks

---
+++ assets/js/design-system/gallery.js
@@ -0,0 +1,20 @@
+class Gallery {
+    constructor (selector) {
+        this.element = document.querySelector(selector);
+        this.figures = document.querySelectorAll('figure');
+        this.fullWidthText = '[full]';
+        
+        this.updateClasses();
+    }
+
+    updateClasses () {
+        this.figures.forEach( function (figure) {
+            if (figure.innerText.includes(this.fullWidthText)) {
+                figure.removeAttribute("aria-label");
+                figure.classList.add("cover");
+            } 
+        }.bind(this));
+    }
+}
+
+export default new Gallery('.block-gallery--grid');
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
 import './theme/';
 import './design-system/logo';
+import './design-system/gallery';
 import './design-system/footer';
